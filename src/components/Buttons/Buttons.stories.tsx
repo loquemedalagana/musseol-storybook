37559fb88d37925a615663913b13c6adebeb114f
loquemedalagana <nadamasquetu012@gmail.com>
2021-03-19 21:41:36 +0900

    update button story

--- src/components/Buttons/Buttons.stories.tsx
+++ src/components/Buttons/Buttons.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
 
-import Button, {IProps} from "./Button";
+import Button, {IProps as ButtonProps} from "./Button";
 
 export default {
   title: 'Musiclab/Button',
@@ -12,10 +12,34 @@ export default {
   },
 } as Meta;
 
-const Template: Story<IProps> = (args) => <Button {...args} />;
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 
+// primary button
 export const Primary = Template.bind({});
 Primary.args = {
+  className: "example-button-typography",
   color: "primary",
+  children: "primary button",
+};
+
+// secondary button (outlined)
+export const Secondary = Template.bind({});
+Secondary.args = {
+  className: "example-button-typography",
+  color: "outlined",
+  children: "outlined button",
+};
+
+export const Success = Template.bind({});
+Success.args = {
+  className: "example-button-typography",
+  color: "success",
+  children: "primary button",
+};
+
+export const Danger = Template.bind({});
+Danger.args = {
+  className: "example-button-typography",
+  color: "danger",
   children: "primary button",
 };
---
+++ src/components/Modals/ModalWrapper.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { IconButton } from "@material-ui/core";
+import { CloseRounded } from "@material-ui/icons";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  position: absolute;
+  border-radius: 7px;
+  max-width: 568px;
+  max-height: 614px;
+  background-color: whitesmoke;
+  z-index: 2020;
+  transform: translate(-50%, -55%);
+  -webkit-box-shadow: 3px 5px 15px 1px #E1E1E1;
+  box-shadow: 3px 5px 15px 1px #E1E1E1;
+
+  @media (max-width: 600px) {
+    max-width: 85vw;
+  }
+  @media (max-width: 992px) {
+    
+  }
+`;
+
+export interface IProps {
+  children: React.ReactNode;
+  modalHeader?: React.ReactNode;
+  onClose: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+const ModalWrapper: React.FC<IProps> = ({ children, onClose }) => {
+  return (
+    <Container className="scrollbar-deep-blue grow-up">
+      <div className="modal-header">
+        <IconButton className="modal-close-button" color="inherit" onClick={onClose}>
+          <CloseRounded className="modal-close" />
+        </IconButton>
+      </div>
+      {children}
+    </Container>
+  );
+};
+
+export default ModalWrapper;
